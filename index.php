b94726b1999a6498ca625e1a11e00158159f5e0d
韩欣宇 <857420437@qq.com>
2018-01-17 14:29:39 +0800

    222

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
  * 2.加载函数库
  * 3.启动框架
  */
-define('hanxinyu',realpath('/'));
+define('IMOOC',realpath('/'));
 define('CORE',IMOOC.'/core');
 define('APP',IMOOC.'/app');
 
 define('DEBUG',true);
 
 if (DEBUG){
-    ini_set('display_error','on');
+    ini_set('display_error','On');
 }else{
-    ini_set('display_error','off');
+    ini_set('display_error','Off');
 }
 
 include CORE.'/commom/function.php';
