1dfd8af83832e1229704039ad8172b5d01a06268
韩欣宇 <857420437@qq.com>
2018-01-18 14:41:46 +0800

    路由

--- core/lib/route.php
+++ core/lib/route.php
@@ -23,12 +23,23 @@ class route
             $patharr = explode('/',trim($path,'/'));
             if (isset($patharr[0])){
                 $this->ctrl = $patharr[0];
+                unset($patharr[0]);
             }
             if (isset($patharr[1])){
                 $this->action = $patharr[1];
+                unset($patharr[1]);
             }else{
                 $this->action = 'index';
             }
+            $count = count($patharr)+2;
+            $i = 2;
+            while ($i<$count){
+                if (isset($patharr[$i+1])) {
+                    $_GET[$patharr[$i]] = $patharr[$i + 1];
+                }
+                $i = $i+2;
+            }
+            p($_GET);
         }else{
             $this->ctrl = 'index';
             $this->ctrl = 'index';
