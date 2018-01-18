a6a4b8425cccb69b8597d0ca3a85ab26a802a6a2
韩欣宇 <857420437@qq.com>
2018-01-18 17:33:35 +0800

    321

--- core/imooc.php
+++ core/imooc.php
@@ -9,7 +9,7 @@ class imooc
     static public function run()
     {
         \core\lib\log::init();
-        \core\lib\log::init('test');
+        \core\lib\log::log('test');
         $route = new \core\lib\route();
         $ctrlClass = $route->ctrl;
         $action = $route->action;
