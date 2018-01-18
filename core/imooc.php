4b7e4aad89611cc5e9f8530e2530327ac6a58660
韩欣宇 <857420437@qq.com>
2018-01-18 10:34:01 +0800

    自动加载4

--- core/imooc.php
+++ core/imooc.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace core;
 class imooc
 {
     public static $classMap = array();
+
     static public function run()
     {
         p('ok');
@@ -12,16 +14,18 @@ class imooc
     static public function load($class)
     {
         //自动加载类库
-        $class = str_replace('\\','/',$class);
-        if (isset($classMap[$class])){
+
+        if (isset($classMap[$class])) {
             return true;
-        }
-        $file = IMOOC.'/'.$class.'.php';
-        if (is_file($file)){
-            include $file;
-            self::$classMap[$class] = $class;
-        }else{
-            return false;
+        } else {
+            $class = str_replace('\\','/',$class);
+            $file = IMOOC . '/' . $class . '.php';
+            if (is_file($file)) {
+                include $file;
+                self::$classMap[$class] = $class;
+            } else {
+                return false;
+            }
         }
     }
 }
