e63528e52b4206586c7d85b2cd7522bb361b44ca
韩欣宇 <857420437@qq.com>
2018-01-19 09:56:47 +0800

    logs

--- core/lib/drive/log/file.php
+++ core/lib/drive/log/file.php
@@ -19,9 +19,9 @@ class file
 
     public function log($message,$file = 'log')
     {
-        if (!is_dir($this->path)){
-            mkdir($this->path,'0777',true);
+        if (!is_dir($this->path.date('YmdH'))){
+            mkdir($this->path.date('YmdH'),'0777',true);
         }
-        file_put_contents($this->path.$file.'.php',date('Y-m-d H:i:s') .json_encode($message).PHP_EOL,FILE_APPEND);
+        return file_put_contents($this->path.date('YmdH').'/'.$file.'.php',date('Y-m-d H:i:s') .json_encode($message).PHP_EOL,FILE_APPEND);
     }
 }
